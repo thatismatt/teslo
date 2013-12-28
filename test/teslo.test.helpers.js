b936c3249ee0f46f8de847092d8d187a23620a5d
Matt Lee <thatismatt@gmail.com>
2013-12-28 10:55:47 +0000

    Fix isNumber and isString assertion helpers.

--- test/teslo.test.helpers.js
+++ test/teslo.test.helpers.js
@@ -4,7 +4,7 @@
 
     // assert helpers
     function simplify (o) { return o.name || o; }
-    function isOfType (type, propFn) {
+    function isOfType (type, propFn, typeFn) {
         // args: env, n
         //    OR env, n, v
         //    OR o
@@ -14,16 +14,19 @@
             var o = aIsEnv ? a.lookup(b) : a;
             var v = aIsEnv ? c : b;
             assert.isDefined(o);
-            assert.ok(o.type, "no type property");
-            assert.equal(o.type, type);
+            var actualType = typeFn ? typeFn(o) : o.type;
+            assert.ok(actualType, "no type");
+            assert.equal(actualType, type);
             if (propFn && v !== undefined) assert.deepEqual(propFn(o), v); }; }
+
+    function jsType (x) { return /\[object (\w*)\]/.exec(Object.prototype.toString.call(x))[1]; }
 
     teslo.test.helpers = {
         isOfType:   isOfType,
         isType:     isOfType("Type", function (o) { return o.name; }),
         isFunction: isOfType("Function"),
-        isNumber:   function (x) { return Object.prototype.toString.call(x) == "[object Number]"; },
-        isString:   function (x) { return Object.prototype.toString.call(x) == "[object String]"; },
+        isNumber:   isOfType("Number", function (x) { return x; }, jsType),
+        isString:   isOfType("String", function (x) { return x; }, jsType),
         isSymbol:   isOfType("Symbol", function (o) { return o.name; }),
         isKeyword:  isOfType("Keyword", function (o) { return o.name; }),
         isArray:    isOfType("Array", function (o) { var r = []; for (var i = 0; i < o.length; i++) { r.push(simplify(o[i])); } return r; } ),
