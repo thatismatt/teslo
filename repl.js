54e46fcf0c5b036eab424f7a3386596659ee4695
Matt Lee <thatismatt@gmail.com>
2013-12-29 09:59:22 +0000

    Node repl fixes.

--- repl.js
+++ repl.js
@@ -6,7 +6,7 @@ function completer (env) {
     return function (line) {
         var parts = line.split(/[\s\()]+/);
         var v = parts[parts.length - 1];
-        var vs = Object.keys(env.frames[0]);
+        var vs = Object.keys(env.frames);
         var hits = vs.filter(function (c) { return c.indexOf(v) === 0; });
         return [hits, v]; }; }
 
@@ -28,7 +28,7 @@ function repl () {
                 var result = teslo.evaluate(line, env);
                 if (result.length) {
                     for (var i = 0; i < result.length; i++) {
-                        if (result[i]) {
+                        if (result[i] !== undefined) {
                             env.def("$" + resultIndex, result[i]);
                             teslo.evaluate("(print $" + resultIndex + ")", env);
                             resultIndex++; } } } }
