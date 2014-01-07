2afa33f682e9091a86a3fb9adc675614e5379b35
Matt Lee <thatismatt@gmail.com>
2014-01-07 22:08:42 +0000

    Add highlighting of def* keywords.

--- etc/teslo.el
+++ etc/teslo.el
@@ -1,4 +1,12 @@
-(define-derived-mode teslo-mode lisp-mode "teslo")
+(defvar teslo-keywords '("def" "defn" "defm" "deft"))
 
-(progn
-  (add-to-list 'auto-mode-alist '("\\.teslo\\'" . teslo-mode)))
+(defvar teslo-font-lock-defaults
+  `(((,(regexp-opt teslo-keywords 'words) . font-lock-keyword-face))))
+
+(define-derived-mode teslo-mode lisp-mode "teslo"
+  "teslo mode is a major mode for editing teslo files"
+  (setq font-lock-defaults teslo-font-lock-defaults))
+
+(add-to-list 'auto-mode-alist '("\\.teslo\\'" . teslo-mode))
+
+(provide 'teslo-mode)
