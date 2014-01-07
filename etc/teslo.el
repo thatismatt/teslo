41d06d96345d4ebca098c9242dd785b35a3f7b6e
Matt Lee <thatismatt@gmail.com>
2014-01-07 23:12:28 +0000

    Add highlighting of `let`, `do`, `match` & def* names.

--- etc/teslo.el
+++ etc/teslo.el
@@ -11,11 +11,13 @@
 
 ;; Provides an editing mode for the teslo programming language.
 
-(defvar teslo-keywords '("def" "defn" "defm" "deft" "fn"))
+(defvar teslo-keywords '("def" "defn" "defm" "deft" "fn" "let" "do" "match"))
 
 (defvar teslo-font-lock-defaults
   `(((,(regexp-opt teslo-keywords 'words) . font-lock-keyword-face)
-     ("\\([A-Z][a-z]*\\)+" . font-lock-type-face))))
+     ("\\([A-Z][a-z]*\\)+" . font-lock-type-face)
+     ("(def[mn]? \\([a-z-]+\\)"
+      (1 font-lock-function-name-face)))))
 
 (define-derived-mode teslo-mode lisp-mode "teslo"
   "teslo mode is a major mode for editing teslo files"
