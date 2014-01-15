f840035886e656d740265ceca9c1bd24d95f2593
Matt Lee <thatismatt@gmail.com>
2014-01-15 08:29:52 +0000

    Increase readability of converted teslo code.

--- bin/convert.js
+++ bin/convert.js
@@ -3,13 +3,19 @@
 var fs = require("fs");
 
 function template (name, src) {
-    return ["(function (teslo) { teslo.", name, " = ", JSON.stringify(src), "; })(this.teslo || require('../'));"].join(""); };
+    return ["(function (teslo) {\nteslo.", name, " = [\n", formatSrc(src), "\n].join('\\n');\n})(this.teslo || require('../'));"].join(""); };
 
 function convert (name) {
     var file = "lib/" + name + ".teslo";
     var src = fs.readFileSync(file, { encoding: "utf8" });
     var jsSrc = template(name, src);
     fs.writeFileSync("lib-js/teslo." + name + ".js", jsSrc); }
+
+function formatSrc (src) {
+    return src
+        .split("\n")
+        .map(JSON.stringify)
+        .join(",\n"); }
 
 if (require.main === module) {
     convert("prelude");
